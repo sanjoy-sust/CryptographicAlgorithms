0322eabf037372e490fe424ec0803abf5c64a4af
Sanjoy Kumer Deb <sanjoyd.cse@gmail.com>
2019-05-28 03:00:42 +0600

    [refs #1] - add key print

--- src/main/java/des/algo/DesAlgorithmImpl.java
+++ src/main/java/des/algo/DesAlgorithmImpl.java
@@ -1,6 +1,7 @@
 package des.algo;
 
 import des.constants.DesConstants;
+import javafx.scene.chart.PieChart;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
@@ -13,57 +14,36 @@ import java.security.spec.AlgorithmParameterSpec;
 
 public class DesAlgorithmImpl implements DesAlgorithm {
 
-    public String encrypt(String data, SecretKey key,String algo) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+    public String encrypt(String data, SecretKey key, String algo) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        return cryptoData(algo, key, Cipher.ENCRYPT_MODE, data.getBytes());
+    }
+
+    public String decrypt(String data, SecretKey key, String algo) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        return cryptoData(algo, key, Cipher.DECRYPT_MODE, DatatypeConverter.parseHexBinary(data));
+    }
+
+    public String encrypt(String data, byte[] sKey, String algo) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        SecretKey key = new SecretKeySpec(sKey, algo);
+        return cryptoData(algo, key, Cipher.ENCRYPT_MODE, data.getBytes());
+    }
+
+    public String decrypt(String data, byte[] sKey, String algo) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        SecretKey key = new SecretKeySpec(sKey, algo);
+        return cryptoData(algo, key, Cipher.DECRYPT_MODE, DatatypeConverter.parseHexBinary(data));
+    }
+
+    private String cryptoData(String algo, SecretKey key, int encryptMode, byte[] bytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance(algo);
-        if(algo.contains("ECB")){
-            cipher.init(Cipher.ENCRYPT_MODE,key);
-        }else{
+        System.out.println("Key supplied : "+ DatatypeConverter.printHexBinary(key.getEncoded()));
+        if (algo.contains("ECB")) {
+            cipher.init(encryptMode, key);
+        } else {
             AlgorithmParameterSpec paramSpec = new IvParameterSpec(DesConstants.iv);
-            cipher.init(Cipher.ENCRYPT_MODE,key,paramSpec);
+            cipher.init(encryptMode, key, paramSpec);
         }
-        byte[] encryptedResult = cipher.doFinal(data.getBytes());
+        byte[] encryptedResult = cipher.doFinal(bytes);
         return DatatypeConverter.printHexBinary(encryptedResult);
     }
 
-    public String decrypt(String data, SecretKey key,String algo) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
-        Cipher cipher = Cipher.getInstance(algo);
-        if(algo.contains("ECB")){
-            cipher.init(Cipher.DECRYPT_MODE,key);
-        }else{
-            AlgorithmParameterSpec paramSpec = new IvParameterSpec(DesConstants.iv);
-            cipher.init(Cipher.DECRYPT_MODE,key,paramSpec);
-        }
-        byte[] decryptedResult = cipher.doFinal(DatatypeConverter.parseHexBinary(data));
-        return DatatypeConverter.printHexBinary(decryptedResult);
-    }
 
-    public String encrypt(String data, byte[] sKey,String algo) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
-
-
-
-        SecretKey key = new SecretKeySpec(sKey, algo);
-        Cipher cipher = Cipher.getInstance(algo);
-        if(algo.contains("ECB")){
-            cipher.init(Cipher.ENCRYPT_MODE,key);
-        }else{
-            AlgorithmParameterSpec paramSpec = new IvParameterSpec(DesConstants.iv);
-            cipher.init(Cipher.ENCRYPT_MODE,key,paramSpec);
-        }
-        byte[] encryptedResult = cipher.doFinal(data.getBytes());
-        return DatatypeConverter.printHexBinary(encryptedResult);
-    }
-
-    public String decrypt(String data, byte[] sKey,String algo) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
-
-        SecretKey key = new SecretKeySpec(sKey, algo);
-        Cipher cipher = Cipher.getInstance(algo);
-        if(algo.contains("ECB")){
-            cipher.init(Cipher.DECRYPT_MODE,key);
-        }else{
-            AlgorithmParameterSpec paramSpec = new IvParameterSpec(DesConstants.iv);
-            cipher.init(Cipher.DECRYPT_MODE,key,paramSpec);
-        }
-        byte[] decryptedResult = cipher.doFinal(DatatypeConverter.parseHexBinary(data));
-        return DatatypeConverter.printHexBinary(decryptedResult);
-    }
 }
